5d1410166bcb071cd8552fbccb7c7db14f8d2fe2
PranavvvSingh <pranavvalt@gmail.com>
2023-11-12 21:42:04 +0530

    hook form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,67 +1,75 @@
-import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+// import { DevTool } from "@hookform/devtools";
+
 
 const Contact = () => {
-  const form = useRef();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const form = useForm();
+  const {register, handleSubmit, formState:{errors}, reset}=form;
+
+  const sendEmail = () => {
+    // e.preventDefault();
     emailjs
       .sendForm("service_ywvdj6d", "template_v9nx5sm",form.current,"IjE3npLZjjNK_35OR")
       .then(
         (result) => {
-          console.log(result.text);
+          console.log(result);
         },
         (error) => {
           console.log(error.text);
         }
       );
-    setName("");
-    setMessage("");
-    setEmail("");
+      reset();
   };
   return (
-    <div id='contact'>
+    <div id="contact">
       <div className="flex flex-col items-center mt-32 m-10">
         <h1 className="text-4xl">Contact</h1>
         <p className="text-xl mt-5">Wanna connect? Nigga hit me up.</p>
       </div>
 
       <div className="w-1/2 mx-auto bg-neutral-950 p-8 rounded-xl border border-rose-700 shadow-md shadow-rose-700">
-        <form ref={form} onSubmit={sendEmail}>
+        <form ref={form} onSubmit={handleSubmit(sendEmail)} noValidate>
           <div className="grid gap-4">
             <h1 className="text-xl text-neutral-400">📩 Email Me</h1>
             <input
               type="text"
               name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
               placeholder="Your Name"
               className="bg-transparent border border-neutral-700 rounded-lg p-3"
+              {...register("name", {
+                required: "Name is required",
+              })}
             />
+            <p className="text-red-600 mt-0">{errors.name?.message}</p>
             <input
               type="email"
               name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               placeholder="Your Email"
               className="bg-transparent border border-neutral-700 rounded-lg p-3"
-              required
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "Invalid Email",
+                },
+              })}
             />
+            <p className="text-red-600 mt-0">{errors.email?.message}</p>
             <textarea
               name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
+              id="message"
               placeholder="Your Message"
               className="bg-transparent border border-neutral-700 rounded-lg p-3"
-              required
+              {...register("message", {
+                required: "Message cannot be empty",
+              })}
             />
+            <p className="text-red-600 mt-0">{errors.message?.message}</p>
             <button
               type="submit"
               value="Send"
-              onClick={sendEmail}
               className="p-3 rounded-lg"
               style={{
                 background: `linear-gradient(166deg, rgba(253,29,29,1) 20%, rgba(224,153,54,1) 100%)`,
@@ -71,6 +79,7 @@ const Contact = () => {
             </button>
           </div>
         </form>
+        {/* <DevTool control={control} /> */}
       </div>
     </div>
   );
