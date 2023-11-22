d379e9f51a8db2efe062765f0e0e8a7ee689688c
PranavvvSingh <pranavvalt@gmail.com>
2023-11-22 16:34:13 +0530

    ref in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,24 +2,34 @@ import emailjs from "@emailjs/browser";
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 // import { DevTool } from "@hookform/devtools";
-
 
 const Contact = () => {
   const form = useForm();
-  const {register, handleSubmit, formState:{errors}, reset}=form;
+  const formjs = useRef();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = form;
   const notify = () => {
     toast.success("Message Sent", {
       position: toast.POSITION.BOTTOM_RIGHT,
-      theme: 'dark',
+      theme: "dark",
       className: "bg-neutral-900 text-white ",
     });
   };
   const sendEmail = () => {
     // e.preventDefault();
     emailjs
-      .sendForm("service_ywvdj6d", "template_v9nx5sm",form.current,"IjE3npLZjjNK_35OR")
+      .sendForm(
+        "service_ywvdj6d",
+        "template_v9nx5sm",
+        formjs.current,
+        "IjE3npLZjjNK_35OR"
+      )
       .then(
         (result) => {
           console.log(result);
@@ -29,7 +39,7 @@ const Contact = () => {
           console.log(error.text);
         }
       );
-      reset();
+    reset();
   };
   return (
     <div id="contact">
@@ -40,58 +50,58 @@ const Contact = () => {
 
       <div className="w-2/5 mx-auto md:bg-neutral-950 p-8 rounded-xl border border-rose-700 shadow-md shadow-rose-700">
         <form
-          ref={form}
+          ref={formjs}
           onSubmit={handleSubmit(sendEmail)}
           noValidate
           className="grid gap-4"
         >
           {/* <div> */}
-            <h1 className="text-xl text-neutral-400">📩 Email Me</h1>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your Name"
-              className="bg-transparent border border-neutral-700 rounded-lg p-3"
-              {...register("name", {
-                required: "Name is required",
-              })}
-            />
-            <p className="text-red-600 mt-0">{errors.name?.message}</p>
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              className="bg-transparent border border-neutral-700 rounded-lg p-3"
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value:
-                    /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  message: "Invalid Email",
-                },
-              })}
-            />
-            <p className="text-red-600 mt-0">{errors.email?.message}</p>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Your Message"
-              className="bg-transparent border border-neutral-700 rounded-lg p-3"
-              {...register("message", {
-                required: "Message cannot be empty",
-              })}
-            />
-            <p className="text-red-600 mt-0">{errors.message?.message}</p>
-            <button
-              type="submit"
-              value="Send"
-              className="p-3 rounded-lg"
-              style={{
-                background: `linear-gradient(166deg, rgba(253,29,29,1) 20%, rgba(224,153,54,1) 100%)`,
-              }}
-            >
-              Send
-            </button>
+          <h1 className="text-xl text-neutral-400">📩 Email Me</h1>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            className="bg-transparent border border-neutral-700 rounded-lg p-3"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          <p className="text-red-600 mt-0">{errors.name?.message}</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            className="bg-transparent border border-neutral-700 rounded-lg p-3"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid Email",
+              },
+            })}
+          />
+          <p className="text-red-600 mt-0">{errors.email?.message}</p>
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Your Message"
+            className="bg-transparent border border-neutral-700 rounded-lg p-3"
+            {...register("message", {
+              required: "Message cannot be empty",
+            })}
+          />
+          <p className="text-red-600 mt-0">{errors.message?.message}</p>
+          <button
+            type="submit"
+            value="Send"
+            className="p-3 rounded-lg"
+            style={{
+              background: `linear-gradient(166deg, rgba(253,29,29,1) 20%, rgba(224,153,54,1) 100%)`,
+            }}
+          >
+            Send
+          </button>
           {/* </div> */}
         </form>
         {/* <DevTool control={control} /> */}
